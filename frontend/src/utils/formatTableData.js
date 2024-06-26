83519ed64b4b0ce03b41e3ac98c98478f9b335af
5arSanti <santiari05@hotmail.com>
2024-06-25 20:20:05 -0500

    formatArray function Fixes

--- frontend/src/utils/formatTableData.js
+++ frontend/src/utils/formatTableData.js
@@ -8,7 +8,7 @@ const formatTableData = (data, category) => {
     const formattedData = {};
     
     for (const subFolder in categoryData) {
-        formattedData[subFolder] = formatTableArray(categoryData[subFolder]);
+        formattedData[subFolder] = categoryData ?? formatTableArray(categoryData[subFolder]);
     }
 
     return formattedData;
